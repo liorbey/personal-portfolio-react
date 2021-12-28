5430dd6eefb0507470aa5d1baf01e3d05e746033
Lior Beyderman <lior.bey95@gmail.com>
2021-12-28 13:42:30 -0500

    Update navbar.js

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import RESUME from "../resume21.pdf"
 import {Navbar, Container} from "react-bootstrap";
 
 function NavbarCustom() {
@@ -8,9 +7,7 @@ function NavbarCustom() {
     <Navbar.Brand href="#home" id='logo'><span> Lior</span><span className="fade">Beyderman</span></Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text id='resume' className='d-none d-lg-block'>
-       <a href={RESUME} target="_blank" style={{textDecoration: 'none', color: 'white'}}>RESUME</a>
-      </Navbar.Text>
+      
     </Navbar.Collapse>
   </Navbar>
   );
